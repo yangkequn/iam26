6ee096680cb1560502086f09ff1441c9818b4d61
yangkequn <yangkequn@gmail.com>
2022-04-23 10:34:53 +0800

    improve: uploading accelerometer

--- src/base/Accelerometer.tsx
+++ src/base/Accelerometer.tsx
@@ -5,7 +5,6 @@ import { MeasureAccelerometer } from "../models/MeasureAccelerometer"
 
 export interface IAcceleration { x: number | null, y: number | null, z: number | null }
 
-let time = new Date().getTime()
 export function Accelerometer({ multiplier = 1000, useGravity = true }: { multiplier?: number, useGravity?: boolean }) {
 
   const [measureIndex, setMeasureIndex] = useState<MeasureAccelerometer>(new MeasureAccelerometer("0", [], [], []))
@@ -16,15 +15,25 @@ export function Accelerometer({ multiplier = 1000, useGravity = true }: { multip
 
   const saveToHistory = (acceleration: IAcceleration) => {
     var now=new Date().getTime()
-    var timespan = now- time
     measureIndex.data.push(Number.parseFloat(acceleration.x.toFixed(5)))
     measureIndex.data.push(Number.parseFloat(acceleration.y.toFixed(5)))
     measureIndex.data.push(Number.parseFloat(acceleration.z.toFixed(5)))
     measureIndex.time.push(now)
     measureIndex.time.push(now)
     measureIndex.time.push(now)
-    // if sec same with last time, just return 
-    if (timespan  < 4000) return
+    let timeLength = measureIndex.time.length
+    if (timeLength < 20) return
+
+    //should monitor at least 3 seconds data points
+    var timePassed=measureIndex.time[timeLength-1]-measureIndex.time[0]
+    if (timePassed<3000)return
+
+    // data point of each seconds should more than 40 points
+    if(timeLength<3*3*40){
+      setMeasureIndex(new MeasureAccelerometer("0", [], [], []))
+      return 
+    }
+
     //压缩时间，第一个值是绝对时间戳，后续是毫秒计数的时间流逝值
     for (let i = measureIndex.time.length-1; i >0; i--) {
       measureIndex.time[i]=measureIndex.time[i]-measureIndex.time[i-1]
